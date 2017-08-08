fbfa86936da809fca741d8445c71683d0542d9fe
Matt Peters <matthew.george.peters@gmail.com>
2017-08-08 17:31:05 -0400

    completed challenge 1

---
+++ Challanges.js
@@ -0,0 +1,16 @@
+// Challange 1:
+// Write three binary functions, add, sub, mul that take two numbers and
+// return their sum, difference and product.
+
+function add(x, y){
+	return x + y;
+}
+function sub(x, y){
+	return x - y;
+}
+function mul(x, y){
+	return x * y;
+}
+add(3, 4);
+sub(3, 4);
+mul(3, 4);
