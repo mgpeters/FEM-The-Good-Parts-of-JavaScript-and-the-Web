bb0be64ef3aa2ed2986722ebf7521e48721c667a
Matt Peters <matthew.george.peters@gmail.com>
2017-08-22 19:13:59 -0400

    added ES2016 version

--- Object BLD template.js
+++ Object BLD template.js
@@ -3,6 +3,8 @@
 // as he finds the this binding (to the callsite) sometimes confusing
 // and therefore, misused.
 
+// ES5 and below:
+
 function constructor(init){
 	var that =  other_constructor(init),
 		member,
@@ -11,4 +13,15 @@ function constructor(init){
 		};
 	that.method = method;
 	return that;
+}
+
+// ES6 and above:
+
+function constructor(spec){
+	let {member} = spec;
+	const {other} = other_constructor(spec);
+	const method = function(){
+		// spec, member, other, method
+	};
+	return Object.freeze({method, other});
 }
